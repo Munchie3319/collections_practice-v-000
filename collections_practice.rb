1945419b54662e494f7e5e98f59cab92cd1d25ae
rosa lee <rosalee0116@gmail.com>
2018-10-03 17:00:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,12 @@ end
 def sort_array_desc(integers)
   integers.sort {|a,b| b<=>a}
 end
+
+def sort_array_char_count(strings)
+  strings.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
